ab6cbb9a5cb3f18c9982d408d31a30a725f569f9
hliu100 <54821319+hliu100@users.noreply.github.com>
2020-02-25 15:04:40 -0800

    3d folder scene and material

---
+++ VR/Assets/XROSUI/Scripts/containerlayer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+
+    public class containerlayer : MonoBehaviour
+    {
+        int max = 4;
+        private float containerObjectRadius=0.05f;
+        private int Rmax;
+        private int Cmax;
+        private int Rlast =0;
+        private int Clast =0;
+        private float buffery=0.2f;
+        private float bufferz=0.3f;
+        public List<containerobject> containerobjectlist = new List<containerobject>();
+        public void AddObject(GameObject go)
+        {
+            containerobject co = go.GetComponent<containerobject>(); 
+            containerobjectlist.Add(co);
+            //co.transform.position = this.transform.position + Vector3.up * 0.5f * containerobjectlist.Count;
+            //Place object
+            int Ri;
+            int Ci;
+            print("set position:");
+            Ri = Rlast;
+            print("Ri: " + Ri);
+            Ci = Clast;
+            print("Ci: " + Ci); 
+            float x = this.transform.position.x + (Ci * buffery) - 0.3f;
+            float y = this.transform.position.y + (Ri * bufferz);
+            float z = this.transform.position.z;
+            co.transform.position = new Vector3(x, y, z);
+            co.transform.SetParent(this.transform);
+            //Calculate Next Position
+            Ci++;
+            print("calculate position");
+            if (Ci >= Cmax)
+            {
+                Ci = 0;
+                //print("Ci = "+Ci);
+                Ri++;
+                //print("Ri= " + Ri);
+            }
+            Rlast = Ri;
+            print("Rlast is" + Rlast);
+            Clast = Ci;
+            print("Clast is" + Clast); 
+            if (Clast >= Cmax && Rlast >= Rmax)
+            {
+                print("is full");
+            }
+        }
+        public bool IsFull()
+        {
+            print("Is Full: " + (containerobjectlist.Count >= max));
+            return containerobjectlist.Count >= max;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+         //x = this.GetComponent<Collider>().bounds.size.x;// PF_layerObject.collider.bounds.size.x;
+         //z = this.GetComponent<Collider>().bounds.size.z; //PF_layerObject.collider.bounds.size.z;
+         //y = this.GetComponent<Collider>().bounds.size.y;// PF_layerObject.collider.bounds.size.y;
+         float a = this.transform.localScale.x;
+         float b = this.transform.localScale.y;
+         float c = this.transform.localScale.z;
+         Cmax = (int)(c / (buffery + (containerObjectRadius * 2)));
+            print("Cmax is "+ Cmax);
+         Rmax = (int)(b / (bufferz + (containerObjectRadius * 2)));
+            print("Rmax is "+ Rmax);
+            print(containerObjectRadius);
+            //IsFull();
+        }
+         // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ VR/Assets/XROSUI/Scripts/containermanager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityEngine.XR.Interaction.Toolkit
+{
+    public class containermanager : MonoBehaviour
+    {
+        public List<containerobject> containerobjectlist = new List<containerobject>();
+        public List<containerlayer> containerlayerlist = new List<containerlayer>();
+        public GameObject PF_containerObject;
+        public GameObject PF_layerObject;
+        //Debug only
+        GameObject CO;
+        void Start()
+        {
+            //TODO for loop to generate layers
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject go = Instantiate(PF_layerObject, this.transform.position + Vector3.forward * 0.5f * i, Quaternion.identity);
+                go.transform.SetParent(this.transform);
+                containerlayerlist.Add(go.GetComponent<containerlayer>());
+            }
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            CheckDistance();
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                AddContainerObject();
+            }
+            //if (Input.GetKeyDown(KeyCode.M))
+            //{
+            //GameObject controllerhand = GameObject.Find("righthand");
+            //print("success find object right hand.");
+            //}
+        }
+        private void Awake()
+        {
+
+        }
+        public void AddContainerObject()
+        {
+            //print("hi");
+            for (int i = 0; i < containerlayerlist.Count; i++)
+            {
+                // print("hi2");
+                if (!containerlayerlist[i].IsFull())
+                {
+                    //print("hi3");
+                    CO = Instantiate(PF_containerObject, transform.position + transform.forward * 2, Quaternion.identity);
+                    containerlayerlist[i].AddObject(CO);
+                    return;
+                }
+            }
+        }
+        #region check distance between the controller and layer, if<1 disappear 
+        public void CheckDistance()
+        {
+            GameObject controllerhand = GameObject.Find("righthand");
+            GameObject disappearlayer = GameObject.Find("ComplexGrabCube");
+            float dis;
+            dis = Vector3.Distance(controllerhand.transform.position, disappearlayer.transform.position);
+            print("dis=" + dis);
+
+            if (dis <= 0.2f)
+            {
+                containerlayerlist[0].gameObject.SetActive(false);
+            }
+            else if (dis > 1.0)
+            {
+                containerlayerlist[0].gameObject.SetActive(true);
+            }
+        }
+        #endregion
+
+
+
+
+    }
+}
