96c566bc99237e388c4c9467efc173279417c8ab
Jessie Jia <ruoxijia@usc.edu>
2020-02-27 12:31:23 -0800

    save for testing

--- VR/Assets/XROSUI/Scripts/SeparateKeyboardCharacterCreator.cs
+++ VR/Assets/XROSUI/Scripts/SeparateKeyboardCharacterCreator.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI; //create public inputfield 
 public class SeparateKeyboardCharacterCreator: KeyboardController
 {
+    public GameObject system;
     public int segments = 10;
     public float xradius = 0.01f;
     public float yradius = 0.01f;
@@ -24,6 +25,8 @@ public class SeparateKeyboardCharacterCreator: KeyboardController
         {
             inputField.text = "";
         }
+        
+
     }
 
     private XRKey CreateKey(string s, GameObject parent, int position)
@@ -39,30 +42,30 @@ public class SeparateKeyboardCharacterCreator: KeyboardController
 
     void CreatePoints()
     {
-        CreateLine(-0.25f, 0.3f +2f, -8f, "qwert");
-        CreateLine(0.25f, 0.3f + 2f, -8f, "yuiop");
-        CreateLine(-0.25f, 0.2f+2f, -8f, "asdfg");
-        CreateLine(0.25f, 0.2f + 2f, -8f, "hjkl;");
-        CreateLine(-0.25f, 0.1f + 2f, -8f, "zxcv");
-        CreateLine(0.25f, 0.1f + 2f, -8f, "bnm,");
+        CreateLine(-0.25f, 0.3f +2f, -8f, -20f, "qwert");
+        CreateLine(0.25f, 0.3f + 2f, -8f, 22f, "yuiop");
+        CreateLine(-0.25f, 0.2f+2f, -8f, -20f, "asdfg");
+        CreateLine(0.25f, 0.2f + 2f, -8f,+22f, "hjkl;");
+        CreateLine(-0.25f, 0.1f + 2f, -8f,-20f, "zxcv");
+        CreateLine(0.25f, 0.1f + 2f, -8f,22f, "bnm,");
     }
 
-    void CreateLine(float offsetX, float offsetY, float offsetZ, string letters)
+    void CreateLine(float offsetX, float offsetY, float offsetZ, float angleOffset,string letters)
     {
         float x;
         float z;
 
-        float angle = 300f - letters.Length * 2f;
-        float tempAngle = angle;
+        float angle = 300f - letters.Length * 2f + angleOffset;
         for (int i = 0; i < (letters.Length); i++)
         {
-            x = Mathf.Sin(Mathf.Deg2Rad * tempAngle) * xradius;
-            z = Mathf.Cos(Mathf.Deg2Rad * tempAngle) * yradius;
+            x = Mathf.Sin(Mathf.Deg2Rad * angle) * xradius;
+            z = Mathf.Cos(Mathf.Deg2Rad * angle) * yradius;
             GameObject go = Instantiate(PF_Key, new Vector3(x + offsetX, offsetY, z + offsetZ), new Quaternion(0, 0, 0, 0)); ;
             go.transform.SetParent(this.transform);
             XRKey key = go.GetComponent<XRKey>();
             key.Setup("" + letters[i], this);
-            tempAngle += (180f / (letters.Length)) % 360;
+            //key.transform.LookAt(system.transform);
+            angle += (180f / (letters.Length)) % 360;
         }
     }
 }
