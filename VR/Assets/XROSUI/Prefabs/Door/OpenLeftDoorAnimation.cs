dde73395899742bf328531a6c395b2f03bc661d1
Hyuck Ju Kim <hyuckjuk@usc.edu>
2020-04-02 14:41:08 -0700

    Open Door + Fade Out Scene
    separate scene for testing opening door and fading out camera

---
+++ VR/Assets/XROSUI/Prefabs/Door/OpenLeftDoorAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLeftDoorAnimation : MonoBehaviour
+{
+
+    [SerializeField] private Animator animationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            animationController.SetBool("openLeftDoor", true);
+            Debug.Log("openLeftDoor");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            animationController.SetBool("openLeftDoor", false);
+        }
+    }
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR/Assets/XROSUI/Prefabs/Door/OpenRightDoorAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenRightDoorAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            animationController.SetBool("openRightDoor", true);
+            Debug.Log("openRightDoor");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Heart"))
+        {
+            animationController.SetBool("openRightDoor", false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
