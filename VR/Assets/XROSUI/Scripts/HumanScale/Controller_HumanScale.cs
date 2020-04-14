e82ebb75ea0cf29129523b028857d5b442506f45
Hanyuan Xiao <xiaohanyuan_3005@icloud.com>
2020-04-13 20:31:46 -0700

    Update arm-length measurement

--- VR/Assets/XROSUI/Scripts/HumanScale/Controller_HumanScale.cs
+++ VR/Assets/XROSUI/Scripts/HumanScale/Controller_HumanScale.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Controller_HumanScale : MonoBehaviour
 {
+    public float leftArmLength = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
