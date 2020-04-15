f047ae6040f7c377ab8bad59b2ac263794d6a4e7
powenyao <powenyao@usc.edu>
2020-04-15 16:06:48 -0700

    Updated Tool_FollowXRRigV2

--- VR/Assets/XROSUI/Scripts/Tools/Tool_FollowXRRigV2.cs
+++ VR/Assets/XROSUI/Scripts/Tools/Tool_FollowXRRigV2.cs
@@ -5,33 +5,57 @@ using UnityEngine;
 public class Tool_FollowXRRigV2 : MonoBehaviour
 {
     public GameObject GO_XRRigCamera;
-    public bool bFollows;
-    public float offset_x = 0.5f;
-    public float offset_y = 0f;
-    public float offset_z = 0f;
+
     public float UpdateFrequencyInSeconds = 0.5f;
+    public float DistanceToStopMoving = 0.1f;
+    public float distanceInFrontOfPlayer = 0.5f;
+
+    public float speed = 1.0f;
+    private Vector3 startPositon;
+    private Vector3 goalPosition;
+    private float startTime;
+    private float journeyLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Canvas.worldCamera = GameObject.Find("XRRig_XROS").GetComponent<UnityEngine.XR.Interaction.Toolkit.XRRig>().cameraGameObject.GetComponent<Camera>();        
+        //GO_XRRigCamera = Core.Ins.XRManager.GetCamera().gameObject;
         GO_XRRigCamera = Camera.main.gameObject;
-
-        FollowCamera();
+        goalPosition = this.transform.position;
+        SetNewGoal();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if (bFollows)
-        {
-            FollowCamera();
-        }
+        //if (LastUpdated + UpdateFrequencyInSeconds < Time.time)
+        //{
+        //}
+        //goalPosition = GO_XRRigCamera.transform.position + GO_XRRigCamera.transform.forward * distanceInFrontOfPlayer;
+        //print(Vector3.Distance(goalPosition, this.transform.position));
+        //if (Vector3.Distance(goalPosition, this.transform.position) > DistanceToStopMoving)
+        //{
+        //    SetNewGoal();
+        //}
+        FollowCamera();
+    }
+
+    void SetNewGoal()
+    {
+        print("Set new goal");
+        //GoalPosition = GO_XRRigCamera.transform.position;
+        startPositon = this.transform.position + this.transform.forward * distanceInFrontOfPlayer;
+        goalPosition = GO_XRRigCamera.transform.position + GO_XRRigCamera.transform.forward * distanceInFrontOfPlayer;
+        journeyLength = Vector3.Distance(startPositon, goalPosition);
+        startTime = Time.time;
     }
 
     void FollowCamera()
     {
-
-        this.transform.position = GO_XRRigCamera.transform.position + new Vector3(offset_x, offset_y, offset_z);
+        //float distCovered = (Time.time - startTime) * speed;
+        //float fractionOfJourney = distCovered / journeyLength;
+        //this.transform.position = Vector3.Lerp(startPositon, goalPosition, fractionOfJourney);
+        this.transform.position = GO_XRRigCamera.transform.position + GO_XRRigCamera.transform.forward * distanceInFrontOfPlayer;
+        this.transform.LookAt(GO_XRRigCamera.transform);
     }
 }
