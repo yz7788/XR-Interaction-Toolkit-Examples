546e593abc9818d3f233af91be6855ea1fa32bb2
huijunjiang <huijunji@usc.edu>
2020-04-21 19:17:28 -0700

    VRE : test volume button and back button for screenshot
    fix the slider of sfx

---
+++ VR/Assets/Tool_TestVolume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Tool_TestVolume : MonoBehaviour
+{
+    public AudioMixer Mixer;
+    public AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestVolume()
+    {
+        
+
+    }
+}
