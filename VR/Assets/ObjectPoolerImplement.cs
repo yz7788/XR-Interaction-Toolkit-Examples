104f0b936b672f5d4072fefd601c9817abbf17c0
zzz7788 <yzhang78@usc.edu>
2020-06-29 20:14:33 -0700

    Add ObjectPool Class & Implementation

---
+++ VR/Assets/ObjectPoolerImplement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolerImplement : MonoBehaviour
+{
+
+    public ObjectPool objectPool;
+    private GameObject testCube;
+
+    private float lastAskTime;
+    GameObject pooledObject;
+
+    //public List<GameObject> pooledObjects = new List<GameObject>();
+
+    
+    #region Singleton Setup
+    private static ObjectPoolerImplement ins = null;
+    public static ObjectPoolerImplement Ins
+    {
+        get
+        {
+            return ins;
+        }
+    }
+
+    private void Awake()
+    {
+        // if the singleton hasn't been initialized yet
+        if (ins != null && ins != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            ins = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    #endregion Singleton Setup
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        objectPool.SetAmount(5);
+        objectPool.Init(testCube);
+
+        lastAskTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastAskTime > 3)
+        {
+            int objectID = 0;
+            if (!objectPool.IsEmpty()) {
+                pooledObject = objectPool.GetPooledObject(ref objectID);
+                Debug.Log("Get " + objectID + "th pooled object");
+                lastAskTime = Time.time;
+            }
+            else
+            {
+                Debug.Log("No more pool object");
+            }      
+        }
+
+        objectPool.MoveForward(0.02f, 0.02f, 0.02f);
+
+        if (!objectPool.IsFull())
+        {
+            foreach (GameObject obj in objectPool.pooledObjects)
+            {
+                if (obj.activeInHierarchy && Vector3.Distance(obj.transform.position, new Vector3(0, 2, 0)) > 50)
+                {
+                    objectPool.ReturnPooledObject(obj);
+                }
+            }
+        }
+        //Debug.Log("SINGLETON UPDATE");
+    }
+
+    public void RegisterObjectPool(ObjectPool op)
+    {
+        Debug.Log("Register Object Pool");
+        objectPool = op;
+    }
+}
---
+++ VR/Assets/XROSUI/Scripts/ObjectPool.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public List<GameObject> pooledObjects = new List<GameObject>();
+    private int amountToPool;
+    public int activeNum = 0;
+
+    Vector3 initPosition = new Vector3(0, 2, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    { 
+        ObjectPoolerImplement.Ins.RegisterObjectPool(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetAmount(int amount)
+    {
+        amountToPool = amount;
+    }
+
+    public void Init(GameObject objectToPool)
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            obj.transform.position = initPosition;
+            pooledObjects.Add(obj);
+            
+        }
+    }
+
+    public GameObject GetPooledObject(ref int objectID)
+    {
+        if (!IsEmpty())
+        {
+            for (int i = 0; i < pooledObjects.Count; i++)
+            {
+                if (!pooledObjects[i].activeInHierarchy)
+                {
+                    pooledObjects[i].SetActive(true);
+                    activeNum++;
+                    objectID = i;
+                    return pooledObjects[i];
+                }
+            }
+        }
+        return null;
+    }
+
+     
+    public void MoveForward(float v1, float v2, float v3)
+    {
+        if (activeNum != 0)
+        {
+            for (int i = 0; i < pooledObjects.Count; i++)
+            {
+                if (pooledObjects[i].activeInHierarchy)
+                {
+                    pooledObjects[i].transform.Translate(v1, v2, v3);
+
+                }
+            }
+        }
+    }
+
+    public void ReturnPooledObject(GameObject obj)
+    {
+        if (activeNum != 0)
+        {
+            obj.transform.position = initPosition;
+            obj.SetActive(false);
+            activeNum--;
+        }
+        else
+        {
+            Debug.Log("Pool is full. Cannot return object");
+        }
+        //pooledObjects.Add(obj);
+    }
+
+    public bool IsEmpty()
+    {
+        if (activeNum == pooledObjects.Count)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsFull()
+    {
+        if (activeNum == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
